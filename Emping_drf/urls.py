c928b4fc072c82e9b1a4eafe4b5c422a1074b023
WqNTqP <kimcharles1122@gmail.com>
2024-09-29 11:59:41 +0800

    book/

--- Emping_drf/urls.py
+++ Emping_drf/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include([
-        path('', include('book.urls')),
+        path('book/', include('book.urls')),
     ]))
 ]
