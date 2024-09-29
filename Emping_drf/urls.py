f7a45a22d6cfa07f2e3b3ea142c94d363a2519d0
WqNTqP <kimcharles1122@gmail.com>
2024-09-29 12:13:28 +0800

    b.admin

--- Emping_drf/urls.py
+++ Emping_drf/urls.py
@@ -14,13 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from .views import SnippetViewSet, AuthorViewSet, BookViewSet, CategoryViewSet
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', SnippetViewSet.as_view({'get': 'list'})),  # Add this line
-    path('snippet', SnippetViewSet.as_view({'get': 'list', 'post': 'post'})),
-    path('author', AuthorViewSet.as_view({'get': 'list', 'post': 'post'})),
-    path('book', BookViewSet.as_view({'get': 'list', 'post': 'post'})),
-    path('category', CategoryViewSet.as_view({'get': 'list', 'post': 'post'})),
+    path('admin/', admin.site.urls),
+    path('api/', include([
+        path('book/', include('book.urls')),
+    ]))
 ]
